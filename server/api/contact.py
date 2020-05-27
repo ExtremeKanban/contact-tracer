985ca13ac08f55ed983fa8aa8dbde0cf5dc2661c
Aaron Hilton <hiltoab@gmail.com>
2020-05-27 09:14:29 -0500

    moved server from php to python

--- server/api/contact.py
+++ server/api/contact.py
@@ -18,6 +18,16 @@ class Contact:
         self.uuids = uuids
 
     def registerInfection(self):
+        #TODO: save the uuids and the NPI of the provider that recorded the positive test
+            #The uuids to be saved should fall in the infections time period based on scientific recommendations
+            #infectionType can be use to extend this system to other types of infections: as of 5/25/2020 this system is designed for tracking COVID-19
+
+        #TODO: check NPI with https://npiregistry.cms.hhs.gov/api/?version=2.1&number=<enter-npi-here>
+            #test NPI = 1023292927
+            #if not found, record in offline location and manually verify
+            #if found, publish the uuids publicly.
+            #possibly notify the NPI holder that the infection was registered.
+
         if self.validateNpi():
             # TODO: Save the Contacts
             print(f"self.npi={self.npi}")
@@ -37,12 +47,16 @@ def hello():
 
 @app.route("/api/contact/register-infection/",  methods = ['POST'])
 def registerInfection():
-    req = request.get_json()
-    contact = Contact(req["npi"])
-    contact.registerInfection()
-    print(req["npi"])
-    return jsonify(req) 
-    #return json.dumps(req), 200, {'ContentType':'application/json'} 
+    try:
+        req = request.get_json()
+        contact = Contact(req["npi"])
+        contact.registerInfection()
+        print(req["npi"])
+        #return jsonify(req) 
+        return json.dumps(req), 201, {'ContentType':'application/json'} 
+    except:
+        return "{\"message\": \"Data is incomplete\"}", 400, {'ContentType':'application/json'} 
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
